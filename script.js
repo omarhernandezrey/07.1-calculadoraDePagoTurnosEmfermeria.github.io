d719bc9def64c229ad982bb2c5725d70e24cc393
omarhernandezrey <hernandezreyomar@gmail.com>
2023-04-28 09:51:34 -0500

    Primer commit creacion de la pagina basica

---
+++ script.js
@@ -0,0 +1,29 @@
+function calcularPago() {
+    const totalDiasMes = parseInt(document.getElementById("totalDiasMes").value);
+    const turnosDia = parseInt(document.getElementById("turnosDia").value);
+    const turnosNoche = parseInt(document.getElementById("turnosNoche").value);
+    const turnosDomingoDia = parseInt(document.getElementById("turnosDomingoDia").value);
+    const turnosDomingoNoche = parseInt(document.getElementById("turnosDomingoNoche").value);
+    const turnosFestivosDia = parseInt(document.getElementById("turnosFestivosDia").value);
+    const turnosFestivosNoche = parseInt(document.getElementById("turnosFestivosNoche").value);
+
+    const valorTurnoDia = 105000;
+    const valorTurnoNoche = 116000;
+    const valorTurnoDomingoDia = 116000;
+    const valorTurnoDomingoNoche = 116000;
+    const valorTurnoFestivosDia = 116000;
+    const valorTurnoFestivosNoche = 116000;
+
+    let totalPagar = 0;
+
+    totalPagar += turnosDia * valorTurnoDia;
+    totalPagar += turnosNoche * valorTurnoNoche;
+    totalPagar += turnosDomingoDia * valorTurnoDomingoDia;
+    totalPagar += turnosDomingoNoche * valorTurnoDomingoNoche;
+    totalPagar += turnosFestivosDia * valorTurnoFestivosDia;
+    totalPagar += turnosFestivosNoche * valorTurnoFestivosNoche;
+
+    const resultadosDiv = document.getElementById("resultados");
+    
+    resultadosDiv.innerHTML = `Total a pagar turnos del mes: $${totalPagar.toLocaleString()} Pesos`;
+}
