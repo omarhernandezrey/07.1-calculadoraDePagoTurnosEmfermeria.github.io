fef0c219c4d39481756ea200d1dc6f445e2d1661
omarhernandezrey <hernandezreyomar@gmail.com>
2023-05-02 15:42:10 -0500

    Nueva funcionalidad calendario logica con javascrpt aplique stilos y modifique html

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+        locale: 'es',
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month'
+        },
+        eventRender: function(event, element) {
+            element.attr('title', event.title);
+        },
+        dayRender: function(date, cell) {
+            if (date.day() === 0 || esFestivo(date)) {
+                cell.addClass('day-number-red');
+            }
+        }
+    });
+
+    generarCalendario();
+});
+
+function obtenerFestivosManualmente() {
+    return {
+        '2023-01': ['2023-01-01'],
+        '2023-03': ['2023-03-20'],
+        '2023-04': ['2023-04-06', '2023-04-07', '2023-04-23'],
+        '2023-05': ['2023-05-01', '2023-05-22'],
+        '2023-06': ['2023-06-12', '2023-06-19'],
+        '2023-07': ['2023-07-03', '2023-07-20'],
+        '2023-08': ['2023-08-07', '2023-08-21'],
+        '2023-10': ['2023-10-16'],
+        '2023-11': ['2023-11-06', '2023-11-13'],
+        '2023-12': ['2023-12-08', '2023-12-25'],
+    };
+}
+function esFestivo(date) {
+    const anioMes = date.format('YYYY-MM');
+    const festivos = obtenerFestivosManualmente()[anioMes] || [];
+    return festivos.includes(date.format('YYYY-MM-DD'));
+}
+
+
+
 function calcularPago() {
     const totalDiasMes = parseInt(document.getElementById("totalDiasMes").value);
     const turnosDia = parseInt(document.getElementById("turnosDia").value);
@@ -24,17 +67,62 @@ function calcularPago() {
     totalPagar += turnosFestivosNoche * valorTurnoFestivosNoche;
 
     const resultadosDiv = document.getElementById("resultados");
-    
+
     resultadosDiv.innerHTML = `Total a pagar turnos del mes: $${totalPagar.toLocaleString()} Pesos`;
+
+    generarCalendario(totalDiasMes, turnosDia, turnosNoche, turnosDomingoDia, turnosDomingoNoche, turnosFestivosDia, turnosFestivosNoche);
 }
+function generarCalendario() {
+    const calendar = $('#calendar');
+    calendar.fullCalendar('removeEvents');
 
-function toggleFooter() {
-    const footer = document.querySelector("footer");
-    const scrollPos = window.pageYOffset || document.documentElement.scrollTop;
+    let currentDate = moment().startOf('month');
+    let eventos = [];
 
-    if (scrollPos > 50) {
-        footer.classList.add("show");
-    } else {
-        footer.classList.remove("show");
+    for (let i = 0; i < currentDate.daysInMonth(); i++) {
+        let currentDay = currentDate.clone().add(i, 'days');
+        let turnoDia = {
+            title: '',
+            start: currentDay.format('YYYY-MM-DD'),
+            color: ''
+        };
+        let turnoNoche = {
+            title: '',
+            start: currentDay.format('YYYY-MM-DD'),
+            color: ''
+        };
+
+        if (i % 2 === 0) { // Día par
+            turnoDia.title = 'Turno Día - Roxana';
+            turnoNoche.title = 'Turno Noche - Roxana';
+        } else { // Día impar
+            turnoDia.title = 'Turno Día - Arlendys';
+            turnoNoche.title = 'Turno Noche - Arlendys';
+        }
+
+        if (currentDay.day() === 0 || esFestivo(currentDay)) { // Es domingo o festivo
+            turnoDia.color = 'gray';
+            turnoNoche.color = 'gray';
+            
+            // Cambiar el título del turno si es domingo o festivo
+            if (esFestivo(currentDay)) {
+                turnoDia.title = turnoDia.title.replace('Turno Día', 'Turno Día Festivo');
+                turnoNoche.title = turnoNoche.title.replace('Turno Noche', 'Turno Noche Festivo');
+            } else if (currentDay.day() === 0) {
+                turnoDia.title = turnoDia.title.replace('Turno Día', 'Turno Día Domingo');
+                turnoNoche.title = turnoNoche.title.replace('Turno Noche', 'Turno Noche Domingo');
+            }
+        } else { // No es domingo ni festivo
+            turnoDia.color = 'orange';
+            turnoNoche.color = 'gray';
+        }
+
+        eventos.push(turnoDia);
+        eventos.push(turnoNoche);
     }
+
+    // Agregar los eventos generados al calendario
+    calendar.fullCalendar('addEventSource', eventos);
+    // Navegar hasta la fecha actual
+    calendar.fullCalendar('gotoDate', moment());
 }
