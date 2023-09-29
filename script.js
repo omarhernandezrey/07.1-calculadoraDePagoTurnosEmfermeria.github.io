a4ae23cee1a0e0e328da87f4df70d41c5370cc8e
omarhernandezrey <hernandezreyomar@gmail.com>
2023-09-28 19:48:05 -0500

    I change nurse shifts on the calendar

--- script.js
+++ script.js
@@ -104,12 +104,13 @@ function generarCalendario() {
 // Actualizar turnos en el calendario enfermeras
 
 if (i % 2 === 0) { // Día impar
-    turnoDia.title = 'Día - Arlendys';
-    turnoNoche.title = 'Noche - Arlendys';
-} else { // Día Par
     turnoDia.title = 'Día - Roxana';
     turnoNoche.title = 'Noche - Roxana';
+} else { // Día Par
+    turnoDia.title = 'Día - Arlendys';
+    turnoNoche.title = 'Noche - Arlendys';
 }
+
 
 
         if (currentDay.day() === 0 || esFestivo(currentDay)) { // Es domingo o festivo
